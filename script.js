b426516c5a5a047f5962271a28b5dd991afe49d8
Esiver <Esiver@users.noreply.github.com>
2020-12-31 16:02:58 +0800

    Can generate QR code of JSON from object

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 
+function fillForm() {
+    const userProfile = new Object();
+    userProfile.dealer = document.getElementById("dealer").value;
+    userProfile.tel = document.getElementById("tel").value;
+    userProfile.store = document.getElementById("store").value;
+    userProfile.model = document.getElementById("model").value;
 
-const input = document.querySelector('dealer');
-const log = document.getElementById("dealerFill");
+    userProfile.address = document.getElementById("address").value;
+    userProfile.email = document.getElementById("email").value;
+    userProfile.date = document.getElementById("date").value;
+    userProfile.serial = document.getElementById("serial").value;
 
-input.addEventListener('change', updateVal);
+   document.getElementById("dealerFill").innerHTML = userProfile.dealer;
+   document.getElementById("telFill").innerHTML = userProfile.tel;
+   document.getElementById("storeFill").innerHTML = userProfile.store;
+   document.getElementById("modelFill").innerHTML = userProfile.model;
 
-function updateVal(e) {
-    log.textContent = e.target.value();
-}
-
-$function(){
-function lol() {
-    console.log(" I work");
-    let dealer = document.getElementById("dealer").value;
-    let tel = document.getElementById("tel").value;
-    let store = document.getElementById("store").value;
-    let model = document.getElementById("model").value;
-
-    let address = document.getElementById("address").value;
-    let email = document.getElementById("email").value;
-    let date = document.getElementById("date").value;
-    let serial = document.getElementById("serial").value;
-    
-   document.getElementById("dealerFill").innerHTML = ""+dealer;
-   document.getElementById("telFill").innerHTML = tel;
-   document.getElementById("storeFill").innerHTML = store;
-   document.getElementById("modelFill").innerHTML = model;
+   document.getElementById("addressFill").innerHTML = userProfile.address;
+   document.getElementById("emailFill").innerHTML = userProfile.email;
+   document.getElementById("dateFill").innerHTML = userProfile.date;
+   document.getElementById("serialFill").innerHTML = userProfile.serial;
    
-   document.getElementById("addressFill").innerHTML = address;
-   document.getElementById("emailFill").innerHTML = email;
-   document.getElementById("dateFill").innerHTML = date;
-   document.getElementById("serialFill").innerHTML = serial;
+   userJson = JSON.stringify(userProfile);
+   
+   let qrcode = new QRCode(document.querySelector("#qrcode"), {
+    text: userJson,
+    width: 300,
+    height: 300,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.M
+});
 
 }
-}
+
+
